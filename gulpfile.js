d44860657200fec177c6d67560a8ee0dcfa63cac
StepaAv <353833@gmail.com>
2018-08-04 11:55:14 +0300

    check point

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
- 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src(['sass/**/*.sass', 'sass/**/*scss'])
-  .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-  .pipe(gulp.dest('css'))
-});
- 
-gulp.task('watch', function(){
-  gulp.watch(['sass/**/*.sass', 'sass/**/*scss'],['sass']);
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
 });
 
-gulp.task('default', ['watch']);
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
 
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/*.html").on("change", server.reload);
+});
